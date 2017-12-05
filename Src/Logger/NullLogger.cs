73f2be7d21fb0f85ea449bfb195ee020b6df4522
David Indrawes <david.indrawes@gmail.com>
2017-12-05 16:21:26 +0000

    adding null implementation for the ILogger

---
+++ Src/Logger/NullLogger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dudes.Logger
+{
+    public class NullLogger : ILogger
+    {
+        public void DEBUG(string message)
+        {
+            
+        }
+
+        public void ERROR(string message)
+        {
+            
+        }
+
+        public void FATAL(string message)
+        {
+            
+        }
+
+        public void INFO(string message)
+        {
+            
+        }
+
+        public void TRACE(string message)
+        {
+            
+        }
+
+        public void WARN(string message)
+        {
+            
+        }
+    }
+}
